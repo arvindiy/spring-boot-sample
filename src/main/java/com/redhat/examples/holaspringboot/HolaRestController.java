e59357fc73c5847283c61bf3bf704caf27a461f3
Arvind <Arvind.Iyer@sephora.com>
2017-11-14 22:29:57 -0800

    simple application property changes

--- src/main/java/com/redhat/examples/holaspringboot/HolaRestController.java
+++ src/main/java/com/redhat/examples/holaspringboot/HolaRestController.java
@@ -3,14 +3,17 @@ package com.redhat.examples.holaspringboot;
 import java.net.InetAddress;
 import java.net.UnknownHostException;
 
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api")
+@ConfigurationProperties(prefix="helloapp")
 public class HolaRestController {
 
+	private String saying;
 	@RequestMapping(method = RequestMethod.GET, value = "/hola", produces = "text/plain")
 	public String hola() throws UnknownHostException {
 		String hostName = null;
@@ -19,6 +22,14 @@ public class HolaRestController {
 		} catch (UnknownHostException e) {
 			hostName = "unknown";
 		}
-		return "Hola Spring Boot de " + hostName;
+		return getSaying() + "Hola Spring Boot de " + hostName;
 	}
+	public String getSaying() {
+		return saying;
+	}
+	public void setSaying(String saying) {
+		this.saying = saying;
+	}
+	
+	
 }
