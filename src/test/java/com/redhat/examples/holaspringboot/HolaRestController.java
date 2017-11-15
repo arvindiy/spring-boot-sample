85c8bd29a372fdfeb0a73206ced4f40a537b1acb
Arvind <Arvind.Iyer@sephora.com>
2017-11-14 20:06:34 -0800

    First service

---
+++ src/test/java/com/redhat/examples/holaspringboot/HolaRestController.java
@@ -0,0 +1,24 @@
+package com.redhat.examples.holaspringboot;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class HolaRestController {
+
+	@RequestMapping(method = RequestMethod.GET, value = "/hola", produces = "text/plain")
+	public String hold() throws UnknownHostException {
+		String hostName = null;
+		try {
+			hostName = InetAddress.getLocalHost().getHostAddress();
+		} catch (UnknownHostException e) {
+			hostName = "unknown";
+		}
+		return "Hola Spring Boot de " + hostName;
+	}
+}
